f4d53aa31f1a559fb7cfa5c3c6392e63e9c60150
hongxu wei <434745765@qq.com>
2018-04-13 21:15:57 +0800

    Create ReverseInteger.cpp

---
+++ ReverseInteger.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int y=0;
+        int n;
+        while( x != 0){
+            n = x%10;
+            // 判断范围
+            if (y > INT_MAX/10 || y < INT_MIN/10){
+                return 0;
+            }
+            y = y*10 + n;
+            x /= 10;
+        }
+        return y;
+    }
+};
