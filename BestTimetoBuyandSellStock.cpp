2947d95a741eecbc6d99be9307eedd13d8ae5d40
hongxu wei <434745765@qq.com>
2018-04-16 21:42:24 +0800

    Create BestTimetoBuyandSellStock.cpp

---
+++ BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;  
+        if(prices.size()==0){  
+            return 0;  
+        }
+        int max = prices[0];  
+        int min = prices[0];  
+        int max_index = 0;  
+        int min_index = 0;  
+        for(int i=0; i<prices.size(); i++){  
+            if(prices[i]<=min){  
+                min = prices[i];  
+                min_index = i;  
+            }else if(prices[i]>=max || (max_index<=min_index)){  
+                max = prices[i];  
+                max_index = i;  
+            }  
+            if(max_index>=min_index){  
+                int temp = max-min;  
+                profit = profit>=temp?profit:temp;  
+            }  
+        }  
+        return profit; 
+
+    }
+};
